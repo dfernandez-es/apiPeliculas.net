1f98c38edfa25e7ecefdbe085b1688a7092791fc
Daniel Fernandez <dff505@hotmail.com>
2023-05-01 18:46:51 +0200

    Añadida la entidad Genero y realizada la migración

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using peliculasapi.Entidades;
+
+namespace peliculasapi
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Genero> Generos { get; set; }
+    }
+}
