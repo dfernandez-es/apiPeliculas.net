3ef7aa9da22fd7d8800b6782ea305e583056f63d
Daniel Fernandez <dff505@hotmail.com>
2023-05-07 20:31:52 +0200

    Creado las migraciones y dbcontext para la relación muchos a muchos entre peliculas, generos y actores

--- ApplicationDbContext.cs
+++ ApplicationDbContext.cs
@@ -13,8 +13,19 @@ namespace peliculasapi
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<PeliculasActores>().HasKey(x => new{x.ActorId, x.PeliculaId});
+
+            builder.Entity<PeliculasGeneros>().HasKey(x => new{x.GeneroId, x.PeliculaId});
+    
+            base.OnModelCreating(builder);
+        }
+        
+
         public DbSet<Genero> Generos { get; set; }
         public DbSet<Actor> Actores { get; set; }
         public DbSet<Pelicula> Peliculas { get; set; }  
+        public DbSet<PeliculasActores> PeliculasActores { get; set; }
+        public DbSet<PeliculasGeneros> PeliculasGeneros { get; set; }
     }
 }
